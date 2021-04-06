bd5053fc4d3e7fc3299d2065d41a3428bf9f20f1
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-04-06 08:19:59 -0300

    added: plano migration

---
+++ database/migrations/03_criar_plano.js
@@ -0,0 +1,23 @@
+exports.up = async function(database, utf8 = false) {
+    return database.schema.hasTable('plano').then(function(exists) {
+        if (!exists) {
+            return database.schema.createTable('plano', table => {
+                if (utf8) { table.collate('utf8_unicode_ci'); }
+                table.string('id', 45).primary();
+                table.string('nome', 80).notNullable();
+                table.string('nome_normalizado', 80).notNullable();
+
+                table.string('valor', 80).notNullable();
+
+                table.integer('status').defaultTo(1);
+                table.integer('deleted').defaultTo(0);
+            });
+        }
+    });
+};
+
+exports.down = async function(database) {
+    return database.schema.hasTable('plano').then(function(exists) {
+        if (exists) { return database.schema.dropTable('plano'); }
+    });
+};
