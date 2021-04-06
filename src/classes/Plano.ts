31998f6db7621e886abde94af98db5e8336e440e
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-04-06 08:20:48 -0300

    added: plano classe

---
+++ src/classes/Plano.ts
@@ -0,0 +1,24 @@
+import Classes from '../System/Classes';
+
+export interface IPlano
+{
+    id: string;
+    nome: string;
+    nome_normalizado: string;
+    valor: number;
+    status: string|number;
+}
+
+class Plano extends Classes {
+    static table = 'plano';
+
+    public static fields = [
+        { name: 'id', type: 'string', required: false },
+        { name: 'nome', type: 'string', required: true },
+        { name: 'nome_normalizado', type: 'string', required: false },
+        { name: 'valor', type: 'number', required: true },
+        { name: 'status', type: 'number', required: false }
+    ];
+}
+
+export default Plano;
