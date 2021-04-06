a45bdb34bc0b2031975d9061390b33cb3a2bf783
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-04-06 08:30:43 -0300

    added: pagamento migratione

---
+++ database/migrations/05_criar_pagamento.js
@@ -0,0 +1,27 @@
+exports.up = async function(database, utf8 = false) {
+    return database.schema.hasTable('pagamento').then(function(exists) {
+        if (!exists) {
+            return database.schema.createTable('pagamento', table => {
+                if (utf8) { table.collate('utf8_unicode_ci'); }
+                table.string('id', 45).primary();
+                table.string('assinatura', 45).notNullable();
+
+                table.string('codigo', 80).notNullable();
+                table.string('tipo_pagamento', 30).notNullable();
+                table.decimal('valor', 14, 2).notNullable();
+                table.string('data', 30).notNullable();
+
+                table.foreign('assinatura').references('id').inTable('assinatura');
+
+                table.integer('status').defaultTo(1);
+                table.integer('deleted').defaultTo(0);
+            });
+        }
+    });
+};
+
+exports.down = async function(database) {
+    return database.schema.hasTable('pagamento').then(function(exists) {
+        if (exists) { return database.schema.dropTable('pagamento'); }
+    });
+};
