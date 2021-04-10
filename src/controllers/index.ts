06ef5066b47572bd9ec49117f0a49b15974b4bc1
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-04-10 14:39:51 -0300

    added: AutorizacaoController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
+import AutorizacaoController from './AutorizacaoController';
 import UsuarioController from './UsuarioController';
 import SessaoController from './SessaoController';
 
 const routes = Router();
 
-[ UsuarioController, SessaoController ].forEach((route) => {
+[ UsuarioController, SessaoController, AutorizacaoController ].forEach((route) => {
     routes.stack = [...routes.stack, ...route.stack];
 });
 
