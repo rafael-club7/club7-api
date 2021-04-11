886de52306b3a95905d7c0256faa15602c563747
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-04-11 15:57:37 -0300

    added: Atualizar Categoria

--- src/controllers/CategoriaEstabelecimentoController.ts
+++ src/controllers/CategoriaEstabelecimentoController.ts
@@ -78,8 +78,92 @@ routes.get('/categoria-estabelecimento', async (req, res) => {
 });
 
 // [GET] => /categoria-estabelecimento/:id
+routes.get('/categoria-estabelecimento/:id', async (req, res) => {
+    const { params } = req;
+    const resp = {
+        status: 0,
+        data: null,
+        errors: []
+    };
+
+    const categoria = <ICategoriaEstabelecimento> await CategoriaEstabelecimento.GetFirst(`id = '${params.id}'`);
+
+    if (categoria === null) {
+        resp.errors.push({
+            msg: 'Categoria não encontrada!'
+        });
+        return res.status(404).send(resp);
+    }
+
+    resp.status = 1;
+    resp.data = categoria;
+    res.send(resp);
+});
 
 // [PUT] => /categoria-estabelecimento/:id
+routes.put('/categoria-estabelecimento/:id', async (req, res) => {
+    const { params, body } = req;
+    const resp = {
+        status: 0,
+        msg: '',
+        data: null,
+        errors: []
+    };
+
+    const categoriaGet = <ICategoriaEstabelecimento> await CategoriaEstabelecimento.GetFirst(`id = '${params.id}'`);
+
+    if (categoriaGet === null) {
+        resp.errors.push({
+            msg: 'Categoria não encontrada!'
+        });
+        return res.status(404).send(resp);
+    }
+
+    const data : { [k: string] : any} = {};
+    const proibidos = ['id', 'status'];
+    let edit = false;
+
+    CategoriaEstabelecimento.fields.forEach(campo => {
+        if (body[campo.name] !== undefined && !proibidos.includes(campo.name)) {
+            data[campo.name] = body[campo.name];
+            if (campo.name === 'nome') {
+                data.nome_normalizado = Util.toNormal(body[campo.name]);
+            }
+            edit = true;
+        }
+    });
+
+    if (!edit) {
+        resp.errors.push({
+            msg: 'Nada para editar'
+        });
+        return res.status(400).send(resp);
+    }
+
+    if(body.nome !== undefined){
+        const nomeExiste = await CategoriaEstabelecimento.GetFirst(`nome = '${body.nome}'`);
+        if(nomeExiste !== null){
+            resp.errors.push({
+                msg: "Já existe uma categoria com esse nome!"
+            });
+            return res.status(400).send(resp);
+        }
+    }
+
+    const update = await CategoriaEstabelecimento.Update(data, `id = '${params.id}'`);
+
+    if (update.status !== 1) {
+        resp.errors.push({
+            msg: 'Não foi possivel atualizar'
+        });
+
+        return res.status(500).send(resp);
+    }
+
+    resp.status = 1;
+    resp.msg = 'Atualizado com sucesso';
+    res.send(resp);
+});
 
 // [DELETE] => /categoria-estabelecimento/:id
 
