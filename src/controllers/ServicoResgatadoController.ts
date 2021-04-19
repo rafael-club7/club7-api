a0157c946084646f806d07c399404c96216f8753
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-04-19 07:34:11 -0300

    fixed: Get servicos resgatados por id

--- src/controllers/ServicoResgatadoController.ts
+++ src/controllers/ServicoResgatadoController.ts
@@ -22,8 +22,8 @@ routes.post(`/servico-resgatado`, async (req, res) => {
     if (resp.errors.length > 0)
         return res.status(400).send(resp);
 
-    const servico = <IServico> await Servico.GetFirst(`id = '${body.servico}'`);
-    if(servico === null){
+    const servico = <IServico>await Servico.GetFirst(`id = '${body.servico}'`);
+    if (servico === null) {
         resp.errors.push({
             msg: "Servico mão encontrado"
         });
@@ -31,53 +31,53 @@ routes.post(`/servico-resgatado`, async (req, res) => {
     }
 
     const hoje = new Date();
-    
-    if(servico.validade !== null && new Date(servico.validade) < hoje){
+
+    if (servico.validade !== null && new Date(servico.validade) < hoje) {
         resp.errors.push({
             msg: "Esse servico não está mais disponível"
         });
-        return res.status (400).send(resp);
+        return res.status(400).send(resp);
     }
 
-    if(getTipoResgateName(Number(servico.tipo_resgate)) === "UNICO"){
-        const jaResgatado = <IServicoResgatado> await ServicoResgatado.GetFirst(`servico = '${body.servico}' AND usuario = '${req.usuario.id}'`);
-        if(jaResgatado !== null){
+    if (getTipoResgateName(Number(servico.tipo_resgate)) === "UNICO") {
+        const jaResgatado = <IServicoResgatado>await ServicoResgatado.GetFirst(`servico = '${body.servico}' AND usuario = '${req.usuario.id}'`);
+        if (jaResgatado !== null) {
             resp.errors.push({
                 msg: "Você já resgatou esse benefício!"
             });
             return res.status(403).send(resp);
         }
     }
-    
-    if(getTipoResgateName(Number(servico.tipo_resgate)) === "MENSAL"){
+
+    if (getTipoResgateName(Number(servico.tipo_resgate)) === "MENSAL") {
         const mesPassado = new Date();
         mesPassado.setMonth(hoje.getMonth() - 1);
         mesPassado.setHours(0);
         mesPassado.setMinutes(0);
         mesPassado.setSeconds(0);
-        
-        const jaResgatado = <IServicoResgatado> await ServicoResgatado.GetFirst(`servico = '${body.servico}' AND usuario = '${req.usuario.id}' AND data >= '${mesPassado.toJSON()}'`);
-        if(jaResgatado !== null){
+
+        const jaResgatado = <IServicoResgatado>await ServicoResgatado.GetFirst(`servico = '${body.servico}' AND usuario = '${req.usuario.id}' AND data >= '${mesPassado.toJSON()}'`);
+        if (jaResgatado !== null) {
             resp.errors.push({
                 msg: "Você já resgatou esse benefício no ultimo mês!"
             });
             return res.status(403).send(resp);
         }
     }
-    
+
     hoje.setHours(0);
     hoje.setMinutes(0);
     hoje.setSeconds(0);
 
-    const servicoResgatadoHoje = <IServicoResgatado> await ServicoResgatado.GetFirst(`estabelecimento = '${servico.estabelecimento}' AND data > '${hoje.toJSON()}' AND usuario = '${req.usuario.id}'`);
-    if(servicoResgatadoHoje !== null){
+    const servicoResgatadoHoje = <IServicoResgatado>await ServicoResgatado.GetFirst(`estabelecimento = '${servico.estabelecimento}' AND data > '${hoje.toJSON()}' AND usuario = '${req.usuario.id}'`);
+    if (servicoResgatadoHoje !== null) {
         resp.errors.push({
             msg: "Você já resgatou um serviço aqui hoje!"
         });
         return res.status(400).send(resp);
     }
 
-    const payload : IServicoResgatado = {
+    const payload: IServicoResgatado = {
         id: Util.GUID(),
         codigo: Util.UID(),
         data: new Date().toJSON(),
@@ -89,7 +89,7 @@ routes.post(`/servico-resgatado`, async (req, res) => {
 
 
     const create = await ServicoResgatado.Create(payload);
-    if(create.status !== 1){
+    if (create.status !== 1) {
         resp.errors.push({
             msg: "Não foi possivel resgatar esse serviço!"
         });
@@ -116,13 +116,13 @@ routes.get(`/servico-resgatado`, async (req, res) => {
     const order_by = String((query.order_by) ? query.order_by : '');
     const limit = String((query.limit) ? query.limit : '');
 
-    if(req.usuario.tipo === 1)
+    if (req.usuario.tipo === 1)
         where = (where !== '') ? `(${where}) AND usuario = '${req.usuario.id}'` : `usuario = '${req.usuario.id}'`;
-    
-    if(req.usuario.tipo === 2)
+
+    if (req.usuario.tipo === 2)
         where = (where !== '') ? `(${where}) AND estabelecimento = '${req.usuario.id}'` : `estabelecimento = '${req.usuario.id}'`;
 
-    const servicos = <IServicoResgatado[]> await ServicoResgatado.Get(where, order_by, limit);
+    const servicos = <IServicoResgatado[]>await ServicoResgatado.Get(where, order_by, limit);
 
     res.set('X-TOTAL-COUNT', await ServicoResgatado.Count(where));
 
@@ -141,16 +141,16 @@ routes.post(`/servico-resgatado/:codigo`, async (req, res) => {
         errors: []
     };
 
-    const servicoResgatado = <IServicoResgatado> await ServicoResgatado.GetFirst(`codigo = '${query.codigo}' AND estabelecimento = '${req.usuario.id}'`);
+    const servicoResgatado = <IServicoResgatado>await ServicoResgatado.GetFirst(`codigo = '${query.codigo}' AND estabelecimento = '${req.usuario.id}'`);
 
-    if(servicoResgatado === null){
+    if (servicoResgatado === null) {
         resp.errors.push({
             msg: "Código não encontrado!"
         });
         return res.status(404).send(resp);
     }
 
-    if(servicoResgatado.status === 2){
+    if (servicoResgatado.status === 2) {
         resp.errors.push({
             msg: "Esse benefício já foi resgatado!"
         });
@@ -158,14 +158,14 @@ routes.post(`/servico-resgatado/:codigo`, async (req, res) => {
     }
 
     const update = await ServicoResgatado.Update({ status: 2 }, `codigo = '${query.codigo}' AND estabelecimento = '${req.usuario.id}'`);
-    if(update.status !== 1){
+    if (update.status !== 1) {
         resp.errors.push({
             msg: "Erro ao registrar o resgate do Serviço!"
         });
         return res.status(500).send(resp);
     }
 
-    const servico = <IServico> await Servico.GetFirst(`id = '${servicoResgatado.servico}'`);
+    const servico = <IServico>await Servico.GetFirst(`id = '${servicoResgatado.servico}'`);
 
     resp.status = 1;
     resp.msg = "Serviço resgatado com sucesso!";
@@ -175,7 +175,7 @@ routes.post(`/servico-resgatado/:codigo`, async (req, res) => {
 
 // [GET] => /servico-resgatado/:id
 routes.get(`/servico-resgatado/:id`, async (req, res) => {
-    const { query } = req;
+    const { params } = req;
     const resp = {
         status: 0,
         msg: '',
@@ -183,15 +183,14 @@ routes.get(`/servico-resgatado/:id`, async (req, res) => {
         errors: []
     };
 
-    let where = `id = '${query.id}'`;
-
-    if(req.usuario.tipo === 1)
+    let where = `id = '${params.id}'`;
+    if (req.usuario.tipo === 1)
         where = `(${where}) AND usuario = '${req.usuario.id}'`;
-    
-    if(req.usuario.tipo === 2)
+
+    if (req.usuario.tipo === 2)
         where = `(${where}) AND estabelecimento = '${req.usuario.id}'`;
 
-    const servico = <IServicoResgatado> await ServicoResgatado.GetFirst(where);
+    const servico = <IServicoResgatado>await ServicoResgatado.GetFirst(where);
 
     resp.status = 1;
     resp.data = servico;
@@ -199,8 +198,9 @@ routes.get(`/servico-resgatado/:id`, async (req, res) => {
 });
 
 // [PUT] => /servico-resgatado/:id
-// [DELETE] => /servico-resgatado/:id
 
+
+// [DELETE] => /servico-resgatado/:id
 
 
 export default routes;
